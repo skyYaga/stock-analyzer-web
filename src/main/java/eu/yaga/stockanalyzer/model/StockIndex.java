87f90040213804391f79e1b4b9cf58fb0fed139f
Andreas Mueller <andreas.mueller@yaga.eu>
2016-11-28 16:37:36 +0100

    add FTSE 100

--- src/main/java/eu/yaga/stockanalyzer/model/StockIndex.java
+++ src/main/java/eu/yaga/stockanalyzer/model/StockIndex.java
@@ -1,15 +1,16 @@
 package eu.yaga.stockanalyzer.model;
 
 /**
- * Created by andreas on 13.11.16.
+ * Enum containing indices
  */
-public enum StockIndex {
+enum StockIndex {
     DAX("^GDAXI", "DAX", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=20735&interval=D1&assetName=DAX&exchange=Xetra&dateStart="),
     MDAX("^MDAXI", "MDAX", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=323547&interval=D1&assetName=MDAX&exchange=Xetra&dateStart="),
     TECDAX("^TECDAX", "TecDAX", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=6623216&interval=D1&assetName=TecDAX&exchange=Xetra&dateStart="),
     SP500("^GSPC", "S&P 500", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=4359526&interval=D1&assetName=S%2526amp%253BP%2520500&exchange=au%C3%9Ferb%C3%B6rslich&dateStart="),
     NASDAQ100("^NDX", "NASDAQ 100", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=325104&interval=D1&assetName=NASDAQ&exchange=Nasdaq+Global+Indices&dateStart="),
-    EUROSTOXX50("^STOXX50E", "EURO STOXX 50", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=193736&interval=D1&assetName=EURO%2520STOXX%252050&exchange=au%C3%9Ferb%C3%B6rslich&dateStart=");
+    EUROSTOXX50("^STOXX50E", "EURO STOXX 50", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=193736&interval=D1&assetName=EURO%2520STOXX%252050&exchange=au%C3%9Ferb%C3%B6rslich&dateStart="),
+    FTSE100("^FTSE", "FTSE 100", "http://www.onvista.de/onvista/boxes/popup/historicalquote.json?notationId=1918069&interval=D1&assetName=FTSE&exchange=au%C3%9Ferb%C3%B6rslich&dateStart=");
 
     private final String symbol;
     private final String description;
