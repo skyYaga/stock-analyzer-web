9f799ea5db93942645e8e4ca182e0aa79ece34ae
Andreas Mueller <andreas.mueller@yaga.eu>
2016-12-05 18:35:36 +0100

    add fundamental data url type + bugfix

--- src/main/java/eu/yaga/stockanalyzer/model/FundamentalDataUrlType.java
+++ src/main/java/eu/yaga/stockanalyzer/model/FundamentalDataUrlType.java
@@ -3,10 +3,11 @@ package eu.yaga.stockanalyzer.model;
 /**
  * Enum that defines the possible URL categories
  */
-enum FundamentalDataUrlType {
+public enum FundamentalDataUrlType {
     EARNINGS_REVISION,
     DIBA_ANALYST_ESTIMATION,
     YAHOO_ANALYST_ESTIMATION,
     QUARTERLY_FIGURES,
+    ONVISTA_FUNDAMENTAL_DATA,
     OTHER
 }
